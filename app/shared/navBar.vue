6aadecd27dc06cd7ad4bb54eeeb0055d7af0ff9f
vianvio <gloomy_wind@hotmail.com>
2015-12-22 15:57:03 +0800

    add nav component & import font-awesome for icons.

---
+++ app/shared/navBar.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav class='nav'></nav>
+</template>
+
+<script>
+console.log('from nav bar~');
+</script>
+
+<style lang="sass">
+@import "./variables.scss";
+
+.nav {
+	
+}
+
+
+</style>
