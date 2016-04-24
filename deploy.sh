1dd0bb7218af51b9d4b82d8e5b4ecace0bb4fef0
vianvio <gloomy_wind@hotmail.com>
2016-04-24 22:34:39 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 HOST=54.238.160.217
 remote_cmd="/home/ubuntu/blogWithVue/remote_server.sh"
 
-ssh -i ~/.ssh/VianTokyo.pem ubuntu@$HOST "remote_cmd"
+ssh -i ~/.ssh/VianTokyo.pem root@$HOST "remote_cmd"
